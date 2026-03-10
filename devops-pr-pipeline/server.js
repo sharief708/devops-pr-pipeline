24e2670771ee814821dbec892128eb0761090bd9
Umar Sharief <umarsharief708@gmail.com>
2026-03-10 15:58:48 +0530

    Test commit for PR pipeline

---
+++ devops-pr-pipeline/server.js
@@ -0,0 +1,10 @@
+const app = require("./src/app");
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log("DevOps pipeline test");
+  console.log("Testing PR branch");
+  console.log("PR pipeline test");
+});
